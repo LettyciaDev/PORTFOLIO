95f65c5678a443046c3a57fbd8c61d6153114a33
LettyciaDev <lettycia.vitoriafranca@gmail.com>
2026-03-07 17:59:13 -0300

    criação da estrutura do site + estilização

---
+++ portfolio/src/app/components/header/index.tsx
@@ -0,0 +1,13 @@
+export function Header(props:any) {
+       <div>
+          <div>modo</div>
+          <nav>
+            <ul>
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>PROJECTS</li>
+              <li>GAME</li>
+            </ul>
+          </nav>
+        </div>
+}
---
+++ portfolio/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+  weight: ["400" , "700"]
+});
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.className} text-orange-400 antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
